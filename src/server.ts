4ee8010dbcb598922d66b228a626cb295a7acf4b
Mostafa Salama <mostafalearning2016@gmail.com>
2019-01-11 23:47:47 +0200

    configure middleware for express app

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import * as path from 'path' ;
+import router from "./routes";
+
+const app = express();
+app.use(express.json()) ;
+app.use(express.urlencoded({extended:false}));
+// static files
+app.use('/public',express.static(path.join(__dirname,'../public')))
+app.use('/',router)
+
+
+export default app ;
